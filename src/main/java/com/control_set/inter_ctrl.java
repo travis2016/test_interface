309e449ade828702978cb8b936c5670ff0611439
travissong <songzc100!>
2020-04-11 16:42:25 +0800

    test

--- src/main/java/com/control_set/inter_ctrl.java
+++ src/main/java/com/control_set/inter_ctrl.java
@@ -1,10 +1,11 @@
 package com.control_set;
 
+import net.sf.json.JSONObject;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -17,11 +18,15 @@ public class inter_ctrl {
     private static final Logger LOGGER = Logger.getLogger(inter_ctrl.class);
 
     @RequestMapping(value="/get_interface.do",method = {RequestMethod.GET,RequestMethod.POST})
-    public String get_interface(HttpServletRequest request){
+    @ResponseBody
+    public JSONObject get_interface(HttpServletRequest request){
         String inter_id = request.getParameter("inter_id");
         String inter_name = request.getParameter("inter_name");
         LOGGER.info("inter_id=="+inter_id);
         LOGGER.info("inter_name=="+inter_name);
-        return "test";
+        JSONObject interface_json = new JSONObject();
+        interface_json.element("inter_name",inter_name);
+        LOGGER.info("interface_json=="+interface_json);
+        return interface_json;
     }
 }
